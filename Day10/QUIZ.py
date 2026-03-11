27be379ac2cfeba6d15f80b1d06aedafae652ecc
krishna vardhan kondapalli <krishnavardhankondapalli@gmail.com>
2026-03-11 21:42:53 +0530

    Add files via upload

---
+++ Day10/QUIZ.py
@@ -0,0 +1,35 @@
+
+score =0
+
+print(f'----------Quiz----------')
+
+print(f'Question no 1: who is your fnd?')
+print('a) kittu ')
+print('b) siddu') 
+print('c) arun ')
+print('d) pavan')
+answer = input(f'your answer: ')
+if answer == 'a':
+    print(f'Correct Answer')
+    score +=1
+else :
+    answer == "bcd"
+    print(f'incorect answer')
+    score -=1
+
+print(f'Question no 1: who is your fnd?')
+print('a) kittu ')
+print('b) siddu') 
+print('c) arun ')
+print('d) pavan')
+answer = input(f'your answer: ')
+if answer == 'a':
+    print(f'Correct Answer')
+    score +=1
+else :
+    answer == "bcd"
+    print(f'incorect answer')
+    score -=1
+
+    if score == 2:
+     print(f'well played, your score {score}')
